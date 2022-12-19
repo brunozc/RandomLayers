e27c54894f083d10e25af5d93385ad20509ad1fa
Bruno Zuada Coelho <Bruno.ZuadaCoelho@deltares.nl>
2022-12-19 13:47:14 +0100

    fixed typo on kriging method

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
                       rf.random_field.reshape(11, 11)[7, :]])
 
     crf = ConditionalRandomFields(ModelName.Gaussian, KrigingMethod.Ordinary,
-                                  theta=[1, 3], anisotropy=[1, 1], angles=[np.pi/6])
+                                  [1, 3], [1, 1], [np.pi/6])
     crf.generate([X, Y], 20, 2, coordinates, data)
 
     # make plot
